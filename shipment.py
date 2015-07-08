a8e8fd38e29d6282243f51b20d6714cda7fe64cf
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2015-07-09 00:43:28 +0530

    Here code will be maintained by Fulfil.IO Inc.

--- shipment.py
+++ shipment.py
@@ -2,8 +2,6 @@
 """
     shipping_dpd.py
 
-    :copyright: (c) 2015 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
 """
 import base64
 from dpd_client import DPDException
